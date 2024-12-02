d8cde511a4cd1dbab495a6b8975faff33cbbbbb6
VisruthSK <67435125+VisruthSK@users.noreply.github.com>
2024-12-01 22:10:57 -0800

    Fixed a small bug, still wrong

--- Day02/02.R
+++ Day02/02.R
@@ -11,20 +11,17 @@ row_safe <- function(vec){
     abs(max_val) <= 3  && abs(min_val) >= 1 && abs(sum(sign(temp))) + 1 == length(vec)
 }
 
-data |> map(row_safe) |> unlist() |> sum()
+print(data |> map(row_safe) |> unlist() |> sum())
 
 row_dampened_safe <- function(vec){
     temp <- na.omit(vec - lag(vec, 1))
     attributes(temp) <- NULL
     
-    # print(temp)
     max_val <- max(abs(temp))
     min_val <- min(abs(temp))
-    max_index <- which.max(temp)+1
-    min_index <- which.min(temp)+1
+    max_index <- which.max(abs(temp))+1
+    min_index <- which.min(abs(temp))+1
 
-    print(temp)
-    
     if(abs(max_val) > 3) {
         print("1")
         vec <- vec[-max_index]
@@ -39,23 +36,9 @@ row_dampened_safe <- function(vec){
         else vec <- vec[-min_index]
     }
 
-    # print(temp)
-    # print(vec)
-    
-    # print(temp)
-
-    # for (i in 2:length(vec)){
-    #     print(vec[i])
-    # }
-
-    # print("NEXT")
-
-    # row_safe(vec) || ()
-    # abs(sum(sign(temp))) + 1 >= length(vec) - 1
-
-    # print(vec)
     row_safe(vec)
 }
 
+# read_lines("./Day02/example.txt") |> str_split(" ") |> map(as.numeric) |> map(row_dampened_safe)
 # read_lines("./Day02/test.txt") |> str_split(" ") |> map(as.numeric) |> map(row_dampened_safe)
-data |> map(row_dampened_safe) |> unlist() |> sum()
+print(data |> map(row_dampened_safe) |> unlist() |> sum())
